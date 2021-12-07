3dca4e7c5a0a330498f616b35c3485a82ad6eb3e
Armando33 <armamdoromeroorbe33@gmail.com>
2021-12-07 17:30:55 -0600

    diseño superior txt y img

--- app/src/main/java/com/app/snapshots/MainActivity.kt
+++ app/src/main/java/com/app/snapshots/MainActivity.kt
@@ -2,20 +2,42 @@ package com.app.snapshots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.app.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
 
+    private lateinit var mActivityFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        setupBottomNav()
     }
 
     private fun setupBottomNav(){
-        var fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().add(R.id.hostFragment, HomeFragment()).commit()
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActivityFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, addFragment, AddFragment::class.java.name)
+            .hide(addFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, homeFragment, HomeFragment::class.java.name).commit()
     }
 }
