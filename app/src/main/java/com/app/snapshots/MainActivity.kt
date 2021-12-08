e0462f8f7b523e2a875fb3e3395e745571c5bb96
Armando33 <armamdoromeroorbe33@gmail.com>
2021-12-07 23:42:29 -0600

    fin

--- app/src/main/java/com/app/snapshots/MainActivity.kt
+++ app/src/main/java/com/app/snapshots/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.app.snapshots
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.app.snapshots.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+import com.firebase.ui.auth.IdpResponse
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 84
 
     private lateinit var mBinding: ActivityMainBinding
 
     private lateinit var mActivityFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
+
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth: FirebaseAuth? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +30,29 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        setupAuth()
         setupBottomNav()
     }
 
-    private fun setupBottomNav(){
+    private fun setupAuth() {
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if (user == null) {
+                startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(
+                            Arrays.asList(
+                                AuthUI.IdpConfig.EmailBuilder().build(),
+                                AuthUI.IdpConfig.GoogleBuilder().build()
+                            )
+                        ).build(), RC_SIGN_IN
+                )
+            }
+        }
+    }
+
+    private fun setupBottomNav() {
         mFragmentManager = supportFragmentManager
 
         val homeFragment = HomeFragment()
@@ -41,26 +71,50 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.hostFragment, homeFragment, HomeFragment::class.java.name).commit()
 
         mBinding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.action_home -> {
-                    mFragmentManager.beginTransaction().hide(mActivityFragment).show(homeFragment).commit()
+                    mFragmentManager.beginTransaction().hide(mActivityFragment).show(homeFragment)
+                        .commit()
                     mActivityFragment = homeFragment
                     true
                 }
                 R.id.action_add -> {
-                    mFragmentManager.beginTransaction().hide(mActivityFragment).show(addFragment).commit()
+                    mFragmentManager.beginTransaction().hide(mActivityFragment).show(addFragment)
+                        .commit()
                     mActivityFragment = addFragment
                     true
                 }
                 R.id.action_profile -> {
-                    mFragmentManager.beginTransaction().hide(mActivityFragment).show(profileFragment).commit()
+                    mFragmentManager.beginTransaction().hide(mActivityFragment)
+                        .show(profileFragment).commit()
                     mActivityFragment = profileFragment
                     true
                 }
-                    else -> false
+                else -> false
             }
         }
 
+        override fun onResume() {
+            super.onResume()
+            mFirebaseAuth?.addAuthStateListener(mAuthListener)
+        }
 
+        override fun onPause() {
+            super.onPause()
+            mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Bienvenido....", Toast.LENGTH_SHORT).show()
+            } else {
+                if(IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
     }
 }
